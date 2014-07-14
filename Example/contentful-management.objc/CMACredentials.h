03ff79074e97d1ee30b45587459f3f0c01bbf9f8
Boris Bügling <boris@icculus.org>
2014-07-14 10:45:36 +0200

    Added file for demo credentials.

---
+++ Example/contentful-management.objc/CMACredentials.h
@@ -0,0 +1,9 @@
+//
+//  CMACredentials.h
+//  contentful-management.objc
+//
+//  Created by Boris Bügling on 14/07/14.
+//  Copyright (c) 2014 Boris Bügling. All rights reserved.
+//
+
+#define CMA_ACCESS_TOKEN        @"your-access-token-here"
